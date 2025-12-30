1a380e29ee1a677a24464615c477a7c35caa8b58
deniz <deniz3082002@gmail.com>
2025-12-30 18:38:57 +0300

    cfg fix

--- btc-feed/services/api-service/cfg.js
+++ btc-feed/services/api-service/cfg.js
@@ -2,6 +2,6 @@ const process = require("process");
 
 module.exports={
     PORT : process.env.PORT || 3000,
-    MONGO_URI : process.env.MONGO_URI || "mongodb://localhost:27017",
+    MONGO_URI : process.env.MONGO_URI || "mongodb://localhost:27017", // mongodb connection string
     MONGO_DB_NAME : process.env.MONGO_DB_NAME || "btc_db"
 };
